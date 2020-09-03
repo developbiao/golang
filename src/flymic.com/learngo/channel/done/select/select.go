e49dfdda02f7fb4061ccdb233891a95c08e3164a
Gong Biao <ggongbiao@gamil.com>
2020-09-03 16:57:33 +0800

    Select producer and consumer test

--- src/flymic.com/learngo/channel/done/select/select.go
+++ src/flymic.com/learngo/channel/done/select/select.go
@@ -19,14 +19,50 @@ func generator() chan int {
 	return out
 }
 
+func worker(id int, c chan int) {
+	for n := range c {
+		time.Sleep(time.Second)
+		fmt.Printf("Worker %d received %d\n", id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
 func main() {
-	var c1, c2 = generator(), generator()
+	var c1, c2 = generator(), generator() // producer
+	var worker = createWorker(0)          // consumer
+
+	var values []int // queue save task
+	tm := time.After(10 * time.Second)
+	tick := time.Tick(time.Second)
+
 	for {
+		var activeWorker chan<- int
+		var activeValue int
+		if len(values) > 0 {
+			activeWorker = worker
+			activeValue = values[0]
+		}
+
 		select {
 		case n := <-c1:
-			fmt.Println("Received from c1:", n)
+			values = append(values, n)
 		case n := <-c2:
-			fmt.Println("Received from c2:", n)
+			values = append(values, n)
+		case activeWorker <- activeValue:
+			values = values[1:]
+
+		case <-time.After(800 * time.Millisecond):
+			fmt.Println("timeout")
+		case <-tick:
+			fmt.Println("queue len = ", len(values))
+		case <-tm:
+			fmt.Println("bye")
+			return
 		}
 	}
 
