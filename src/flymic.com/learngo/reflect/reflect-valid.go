18bc67997d26153df575f312e24a1f51a48c69a0
Gong Biao <developbiao@gmail.com>
2020-09-07 16:37:31 +0800

    Learning go reflect function

---
+++ src/flymic.com/learngo/reflect/reflect-valid.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type haojiahuo2 struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	var a *int                              // 声明一个变量a为nil的空指针
+	fmt.Println(reflect.ValueOf(a).IsNil()) // 判断是否是nil 返回true
+
+	// 当reflect.Value不包含任何信息，值为nil的时候IsValid()就返回false
+	fmt.Println(reflect.ValueOf(nil).IsValid())
+
+}
